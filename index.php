7aea4e63eb2a44d28ceff5dbef367ddeb49965d0
Mahmudul Hasan <mahmudul16-546@diu.edu.bd>
2021-06-16 13:05:19 +0600

    Delete index.php

--- index.php
+++
@@ -1,4 +0,0 @@
-Hello world !!!
-<script data-cfasync="false">
-(function(W,i,s,e,P,o,p){W['WisePopsObject']=P;W[P]=W[P]||function(){(W[P].q=W[P].q||[]).push(arguments)},W[P].l=1*new Date();o=i.createElement(s),p=i.getElementsByTagName(s)[0];o.defer=1;o.src=e;p.parentNode.insertBefore(o,p)})(window,document,'script','//loader.wisepops.com/get-loader.js?v=1&site=YUQNUhA8jy','wisepops');
-</script>
